4e4d6cc1a2d7624e14666991d3d9eba7734e2d35
bang-koon <dol7966@gmail.com>
2024-10-25 12:57:37 +0900

    feat(feature/#5): 모듈 캐싱이 일어나기에 전역변수 대신 싱글톤으로 변경

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -4,21 +4,18 @@ import { MongoClient } from "mongodb";
 const URI = process.env.DB_URI as string;
 const options = {};
 
-let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 if (!URI) {
   throw new Error("Please add your Mongo URI");
 }
 
-declare global {
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
+function getClientPromise() {
+  if (!clientPromise) {
+    const client = new MongoClient(URI, options);
+    clientPromise = client.connect();
+  }
+  return clientPromise;
 }
 
-if (!global._mongoClientPromise) {
-  client = new MongoClient(URI, options);
-  global._mongoClientPromise = client.connect();
-}
-clientPromise = global._mongoClientPromise;
-
-export default clientPromise;
+export default getClientPromise();
