b8bf8f9dabab44587be1343e3294d6f4e3fd2975
bang-koon <dol7966@gmail.com>
2025-01-15 19:42:10 +0900

    fix(fix/#21): totalCalculator의 armor에도 재료가 5배로 들어가도록 변경

--- app/lib/refine/totalCalculator.ts
+++ app/lib/refine/totalCalculator.ts
@@ -11,14 +11,18 @@ export default function totalCalculator(
   const weapon = calculator(currentLevel, targetLevel, "weapon", materials);
   const armor = calculator(currentLevel, targetLevel, "armor", materials);
 
+  armor.cost *= 5;
+  for (let key in armor.materials) {
+    armor.materials[key] *= 5;
+  }
+
   const total = {
-    cost: weapon.cost + armor.cost * 5,
+    cost: weapon.cost + armor.cost,
     materials: { ...weapon.materials },
   };
 
   for (let key in armor.materials) {
-    total.materials[key] =
-      (total.materials[key] || 0) + armor.materials[key] * 5;
+    total.materials[key] = (total.materials[key] || 0) + armor.materials[key];
   }
 
   return { total, weapon, armor };
