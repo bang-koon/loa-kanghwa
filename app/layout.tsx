c080276621d945ae139e0e190423ba5bf7765526
bang-koon <dol7966@gmail.com>
2025-02-11 03:20:14 +0900

    chore: 메타데이터 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,27 @@
+import { Metadata } from "next";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
-import Head from "next/head";
+
+export const metadata: Metadata = {
+  title: "로아쿤 - 로스트아크 재련 비용 계산기",
+  description: "로아 재련 비용 계산기",
+  openGraph: {
+    title: "로아쿤 - 로스트아크 재련 비용 계산기",
+    description: "로아 재련 비용 계산기",
+    url: "https://loa-koon.co.kr",
+    siteName: "로아쿤",
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    title: "로아쿤 - 로스트아크 재련 비용 계산기",
+    description: "로아 재련 비용 계산기",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -10,28 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <title>로아쿤 - 로스트아크 재련 비용 계산기</title>
-        <meta name="description" content="로아쿤 - 로아 재련 비용 계산기" />
-        <meta name="robots" content="index, follow" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta charSet="utf-8" />
-        <meta property="og:site_name" content="로아쿤" />
-        <meta property="og:locale" content="ko_KR" />
-        <meta
-          property="og:title"
-          content="로아쿤 - 로스트아크 재련 비용 계산기"
-        />
-        <meta property="og:description" content="로아 재련 비용 계산기" />
-        <meta property="og:type" content="website" />
-
-        <meta property="og:url" content="https://loa-koon.co.kr" />
-        <meta
-          name="twitter:title"
-          content="로아쿤 - 로스트아크 재련 비용 계산기"
-        />
-        <meta name="twitter:description" content="로아 재련 비용 계산기" />
-      </Head>
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-7085791456313815" />
+      </head>
       <body>
         <GoogleTagManager gtmId={`${process.env.GA}`} />
         <GoogleAnalytics gaId={`${process.env.GT}`} />
