b970112df95e413f6d5e9e247c26e3ac8dbfb53a
bang-koon <dol7966@gmail.com>
2025-10-01 04:13:44 +0900

    chore(#48): 레이드 이미지 추가

--- app/components/Reward/Reward.tsx
+++ app/components/Reward/Reward.tsx
@@ -40,7 +40,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 1,
       name: "발탄",
       difficulty: "Normal",
-      image: "",
+      image: "발탄.webp",
       itemLevel: "아이템 레벨 1415",
       gates: [
         {
@@ -61,7 +61,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 2,
       name: "발탄",
       difficulty: "Hard",
-      image: "",
+      image: "발탄.webp",
       itemLevel: "아이템 레벨 1445",
       gates: [
         {
@@ -82,7 +82,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 3,
       name: "비아키스",
       difficulty: "Normal",
-      image: "",
+      image: "비아키스.webp",
       itemLevel: "아이템 레벨 1430",
       gates: [
         {
@@ -103,7 +103,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 4,
       name: "비아키스",
       difficulty: "Hard",
-      image: "",
+      image: "비아키스.webp",
       itemLevel: "아이템 레벨 1460",
       gates: [
         {
@@ -124,7 +124,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 5,
       name: "쿠크세이튼",
       difficulty: null,
-      image: "",
+      image: "쿠크세이튼.webp",
       itemLevel: "아이템 레벨 1475",
       gates: [
         {
@@ -151,7 +151,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 6,
       name: "아브렐슈드",
       difficulty: "Normal",
-      image: "",
+      image: "아브렐슈드.webp",
       itemLevel: "아이템 레벨 1490",
       gates: [
         {
@@ -184,7 +184,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 7,
       name: "아브렐슈드",
       difficulty: "Hard",
-      image: "",
+      image: "아브렐슈드.webp",
       itemLevel: "아이템 레벨 1540",
       gates: [
         {
@@ -217,7 +217,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 8,
       name: "일리아칸",
       difficulty: "Normal",
-      image: "",
+      image: "일리아칸.webp",
       itemLevel: "아이템 레벨 1580",
       gates: [
         {
@@ -244,7 +244,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 9,
       name: "일리아칸",
       difficulty: "Hard",
-      image: "",
+      image: "일리아칸.webp",
       itemLevel: "아이템 레벨 1600",
       gates: [
         {
@@ -271,7 +271,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 10,
       name: "카양겔",
       difficulty: "Normal",
-      image: "",
+      image: "카양겔.webp",
       itemLevel: "아이템 레벨 1540",
       gates: [
         {
@@ -298,7 +298,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 11,
       name: "카양겔",
       difficulty: "Hard",
-      image: "",
+      image: "카양겔.webp",
       itemLevel: "아이템 레벨 1580",
       gates: [
         {
@@ -325,7 +325,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 12,
       name: "상아탑",
       difficulty: "Normal",
-      image: "",
+      image: "상아탑.jpg",
       itemLevel: "아이템 레벨 1600",
       gates: [
         {
@@ -352,7 +352,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 13,
       name: "상아탑",
       difficulty: "Hard",
-      image: "",
+      image: "상아탑.webp",
       itemLevel: "아이템 레벨 1620",
       gates: [
         {
@@ -379,7 +379,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 14,
       name: "카멘",
       difficulty: "Normal",
-      image: "",
+      image: "카멘.webp",
       itemLevel: "아이템 레벨 1610",
       gates: [
         {
@@ -406,7 +406,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 15,
       name: "카멘",
       difficulty: "Hard",
-      image: "",
+      image: "카멘.webp",
       itemLevel: "아이템 레벨 1630",
       gates: [
         {
@@ -441,7 +441,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 16,
       name: "베히모스",
       difficulty: null,
-      image: "",
+      image: "베히모스.webp",
       itemLevel: "아이템 레벨 1640",
       gates: [
         {
@@ -462,7 +462,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 17,
       name: "에키드나",
       difficulty: "Normal",
-      image: "",
+      image: "에키드나.webp",
       itemLevel: "아이템 레벨 1620",
       gates: [
         {
@@ -483,7 +483,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 18,
       name: "에키드나",
       difficulty: "Hard",
-      image: "",
+      image: "에키드나.webp",
       itemLevel: "아이템 레벨 1640",
       gates: [
         {
@@ -504,7 +504,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 19,
       name: "에기르",
       difficulty: "Normal",
-      image: "",
+      image: "에기르.webp",
       itemLevel: "아이템 레벨 1660",
       gates: [
         {
@@ -525,7 +525,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 20,
       name: "에기르",
       difficulty: "Hard",
-      image: "",
+      image: "에기르.webp",
       itemLevel: "아이템 레벨 1680",
       gates: [
         {
@@ -546,7 +546,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 21,
       name: "아브(2막)",
       difficulty: "Normal",
-      image: "",
+      image: "아브2막.webp",
       itemLevel: "아이템 레벨 1670",
       gates: [
         {
@@ -567,7 +567,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 22,
       name: "아브(2막)",
       difficulty: "Hard",
-      image: "",
+      image: "아브(2막).webp",
       itemLevel: "아이템 레벨 1690",
       gates: [
         {
@@ -588,7 +588,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 23,
       name: "모르둠",
       difficulty: "Normal",
-      image: "",
+      image: "모르둠.jpg",
       itemLevel: "아이템 레벨 1680",
       gates: [
         {
@@ -615,7 +615,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 24,
       name: "모르둠",
       difficulty: "Hard",
-      image: "",
+      image: "모르둠.jpg",
       itemLevel: "아이템 레벨 1700",
       gates: [
         {
@@ -642,7 +642,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 25,
       name: "아르모체",
       difficulty: "Normal",
-      image: "",
+      image: "아르모체.webp",
       itemLevel: "아이템 레벨 1700",
       gates: [
         { gate: 1, gold: 12500, bonusGold: 4000, materials: [] },
@@ -653,7 +653,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 26,
       name: "아르모체",
       difficulty: "Hard",
-      image: "",
+      image: "아르모체.webp",
       itemLevel: "아이템 레벨 1720",
       gates: [
         { gate: 1, gold: 15000, bonusGold: 4800, materials: [] },
@@ -664,7 +664,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 27,
       name: "카제로스",
       difficulty: "Normal",
-      image: "",
+      image: "카제로스.webp",
       itemLevel: "아이템 레벨 1710",
       gates: [
         { gate: 1, gold: 14000, bonusGold: 4480, materials: [] },
@@ -675,7 +675,7 @@ const mockRaidData: { "3t": Raid[]; "4t": Raid[] } = {
       id: 28,
       name: "카제로스",
       difficulty: "Hard",
-      image: "",
+      image: "카제로스.webp",
       itemLevel: "아이템 레벨 1730",
       gates: [
         { gate: 1, gold: 17000, bonusGold: 5440, materials: [] },
@@ -691,12 +691,8 @@ const Reward = () => {
   const [activeTab, setActiveTab] = useState<"3t" | "4t">("3t");
 
   const initialRaid = mockRaidData[activeTab][0];
-  const [selectedRaidName, setSelectedRaidName] = useState<string>(
-    initialRaid.name
-  );
-  const [selectedDifficulty, setSelectedDifficulty] = useState<
-    "Normal" | "Hard" | null
-  >(initialRaid.difficulty);
+  const [selectedRaidName, setSelectedRaidName] = useState<string>(initialRaid.name);
+  const [selectedDifficulty, setSelectedDifficulty] = useState<"Normal" | "Hard" | null>(initialRaid.difficulty);
 
   const handleTabClick = (tab: "3t" | "4t") => {
     setActiveTab(tab);
@@ -717,10 +713,7 @@ const Reward = () => {
       <div className={styles.materialsWrapper}>
         {materials.map((m, i) => (
           <div key={i} className={styles.materialItem}>
-            <div
-              className={styles.materialIcon}
-              style={{ backgroundImage: `url(${m.image})` }}
-            ></div>
+            <div className={styles.materialIcon} style={{ backgroundImage: `url(${m.image})` }}></div>
             <span>x {m.count.toLocaleString()}</span>
           </div>
         ))}
@@ -743,8 +736,7 @@ const Reward = () => {
         acc.bonusGold += gate.bonusGold || 0;
         if (gate.materials) {
           gate.materials.forEach((mat: Material) => {
-            acc.materials[mat.image] =
-              (acc.materials[mat.image] || 0) + mat.count;
+            acc.materials[mat.image] = (acc.materials[mat.image] || 0) + mat.count;
           });
         }
         return acc;
@@ -752,9 +744,7 @@ const Reward = () => {
       { gold: 0, bonusGold: 0, materials: {} }
     );
 
-    const totalMaterialsForRender: Material[] = Object.keys(
-      totals.materials
-    ).map(image => ({
+    const totalMaterialsForRender: Material[] = Object.keys(totals.materials).map(image => ({
       image,
       count: totals.materials[image],
     }));
@@ -771,76 +761,36 @@ const Reward = () => {
         {raid.gates.map((g: Gate, index: number) => (
           <div key={index} className={styles.row}>
             <div className={styles.gateCol}>{g.gate}</div>
-            <div className={styles.materialCol}>
-              {renderMaterials(g.materials)}
-            </div>
-            <div className={styles.materialCol}>
-              {renderMaterials(g.materials)}
-            </div>
+            <div className={styles.materialCol}>{renderMaterials(g.materials)}</div>
+            <div className={styles.materialCol}>{renderMaterials(g.materials)}</div>
             <div className={styles.goldCol}>
               <div className={styles.goldContentWrapper}>
-                <span className={styles.goldText}>
-                  {g.bonusGold ? g.bonusGold.toLocaleString() : "-"}
-                </span>
-                <Image
-                  className={styles.goldIcon}
-                  src="/reward/coin.svg"
-                  alt="골드"
-                  width={20}
-                  height={20}
-                />
+                <span className={styles.goldText}>{g.bonusGold ? g.bonusGold.toLocaleString() : "-"}</span>
+                <Image className={styles.goldIcon} src="/reward/coin.svg" alt="골드" width={20} height={20} />
               </div>
             </div>
             <div className={styles.goldCol}>
               <div className={styles.goldContentWrapper}>
-                <span className={styles.goldText}>
-                  {g.gold ? g.gold.toLocaleString() : "-"}
-                </span>
-                <Image
-                  className={styles.goldIcon}
-                  src="/reward/coin.svg"
-                  alt="골드"
-                  width={20}
-                  height={20}
-                />
+                <span className={styles.goldText}>{g.gold ? g.gold.toLocaleString() : "-"}</span>
+                <Image className={styles.goldIcon} src="/reward/coin.svg" alt="골드" width={20} height={20} />
               </div>
             </div>
           </div>
         ))}
         <div className={`${styles.row}`}>
           <div className={styles.gateCol}>합계</div>
-          <div className={styles.materialCol}>
-            {renderMaterials(totalMaterialsForRender)}
-          </div>
-          <div className={styles.materialCol}>
-            {renderMaterials(totalMaterialsForRender)}
-          </div>
+          <div className={styles.materialCol}>{renderMaterials(totalMaterialsForRender)}</div>
+          <div className={styles.materialCol}>{renderMaterials(totalMaterialsForRender)}</div>
           <div className={styles.goldCol}>
             <div className={styles.goldContentWrapper}>
-              <span className={styles.goldText}>
-                {totals.bonusGold.toLocaleString()}
-              </span>
-              <Image
-                className={styles.goldIcon}
-                src="/reward/coin.svg"
-                alt="골드"
-                width={20}
-                height={20}
-              />
+              <span className={styles.goldText}>{totals.bonusGold.toLocaleString()}</span>
+              <Image className={styles.goldIcon} src="/reward/coin.svg" alt="골드" width={20} height={20} />
             </div>
           </div>
           <div className={styles.goldCol}>
             <div className={styles.goldContentWrapper}>
-              <span className={styles.goldText}>
-                {totals.gold.toLocaleString()}
-              </span>
-              <Image
-                className={styles.goldIcon}
-                src="/reward/coin.svg"
-                alt="골드"
-                width={20}
-                height={20}
-              />
+              <span className={styles.goldText}>{totals.gold.toLocaleString()}</span>
+              <Image className={styles.goldIcon} src="/reward/coin.svg" alt="골드" width={20} height={20} />
             </div>
           </div>
         </div>
@@ -848,10 +798,7 @@ const Reward = () => {
     );
   };
 
-  const raidNamesForCurrentTab = useMemo(
-    () => [...new Set(mockRaidData[activeTab].map(raid => raid.name))],
-    [activeTab]
-  );
+  const raidNamesForCurrentTab = useMemo(() => [...new Set(mockRaidData[activeTab].map(raid => raid.name))], [activeTab]);
 
   const availableDifficulties = useMemo(
     () =>
@@ -863,28 +810,17 @@ const Reward = () => {
   );
 
   const raidToDisplay = useMemo(
-    () =>
-      allRaids.find(
-        raid =>
-          raid.name === selectedRaidName &&
-          raid.difficulty === selectedDifficulty
-      ),
+    () => allRaids.find(raid => raid.name === selectedRaidName && raid.difficulty === selectedDifficulty),
     [selectedRaidName, selectedDifficulty]
   );
 
   return (
     <div className={styles.container}>
       <div className={styles.tabContainer}>
-        <button
-          className={`${styles.tab} ${activeTab === "3t" ? styles.active : ""}`}
-          onClick={() => handleTabClick("3t")}
-        >
+        <button className={`${styles.tab} ${activeTab === "3t" ? styles.active : ""}`} onClick={() => handleTabClick("3t")}>
           3티어
         </button>
-        <button
-          className={`${styles.tab} ${activeTab === "4t" ? styles.active : ""}`}
-          onClick={() => handleTabClick("4t")}
-        >
+        <button className={`${styles.tab} ${activeTab === "4t" ? styles.active : ""}`} onClick={() => handleTabClick("4t")}>
           4티어
         </button>
       </div>
@@ -897,28 +833,36 @@ const Reward = () => {
         mousewheel={{ sensitivity: 1.5 }}
         className={styles.raidSelector}
       >
-        {raidNamesForCurrentTab.map(name => (
-          <SwiperSlide
-            key={name}
-            className={`${styles.raidSlide} ${
-              selectedRaidName === name ? styles.selected : ""
-            }`}
-            onClick={() => handleRaidNameClick(name)}
-          >
-            <div
-              className={styles.raidImagePlaceholder}
-              // style={{ backgroundImage: `url(${raid.image})` }} // Find appropriate image
-            ></div>
-            <span>{name}</span>
-          </SwiperSlide>
-        ))}
+        {raidNamesForCurrentTab.map(name => {
+          const raidForName = allRaids.find(raid => raid.name === name);
+          return (
+            <SwiperSlide
+              key={name}
+              className={`${styles.raidSlide} ${selectedRaidName === name ? styles.selected : ""}`}
+              onClick={() => handleRaidNameClick(name)}
+            >
+              <div
+                className={styles.raidImagePlaceholder}
+                style={{
+                  backgroundImage: raidForName ? `url(/raid/${raidForName.image})` : "none",
+                }}
+              ></div>
+              <span>{name}</span>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
 
       {raidToDisplay && (
         <div className={styles.raidContentBox}>
           <div className={styles.raidHeader}>
             <div className={styles.raidHeaderLeft}>
-              <div className={styles.raidImage}></div>
+              <div
+                className={styles.raidImage}
+                style={{
+                  backgroundImage: `url(/raid/${raidToDisplay.image})`,
+                }}
+              ></div>
               <div className={styles.raidInfo}>
                 <span className={styles.raidName}>{selectedRaidName}</span>
                 <span className={styles.itemLevel}>{raidToDisplay.itemLevel}</span>
@@ -927,17 +871,13 @@ const Reward = () => {
             {availableDifficulties.length > 1 && (
               <div className={styles.difficultyButtons}>
                 <button
-                  className={`${styles.difficultyButton} ${
-                    selectedDifficulty === "Normal" ? styles.active : ""
-                  }`}
+                  className={`${styles.difficultyButton} ${selectedDifficulty === "Normal" ? styles.active : ""}`}
                   onClick={() => setSelectedDifficulty("Normal")}
                 >
                   노말
                 </button>
                 <button
-                  className={`${styles.difficultyButton} ${
-                    selectedDifficulty === "Hard" ? styles.active : ""
-                  }`}
+                  className={`${styles.difficultyButton} ${selectedDifficulty === "Hard" ? styles.active : ""}`}
                   onClick={() => setSelectedDifficulty("Hard")}
                 >
                   하드
@@ -948,12 +888,7 @@ const Reward = () => {
           {renderRaidTable(raidToDisplay)}
           <div className={styles.notice}>
             <div className={styles.noticeHeader}>
-              <Image
-                src="/reward/infoCircle.svg"
-                alt="주의"
-                width={14}
-                height={14}
-              />
+              <Image src="/reward/infoCircle.svg" alt="주의" width={14} height={14} />
               <span className={styles.noticeTitle}>Notice</span>
             </div>
             <div className={styles.noticeContent}>
