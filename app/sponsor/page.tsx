1d61f8060d2b5e5b27ac0103b6e36381a3b24cf4
bang-koon <dol7966@gmail.com>
2025-10-03 10:53:36 +0900

    feat(#50): 문의, 후원하기 qr 추가

--- app/sponsor/page.tsx
+++ app/sponsor/page.tsx
@@ -1,10 +1,21 @@
 import PageLayout from "../components/PageLayout/PageLayout";
+import Image from "next/image";
 
 export default function SponsorPage() {
   return (
     <PageLayout title="후원하기">
-      <p>서비스 유지 및 발전을 위한 후원은 언제나 환영합니다.</p>
-      <p>후원 방법은 여기에 추가될 예정입니다.</p>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <p>ㅤ카카오 페이 송금 qr입니다. 조금의 마음도 감사히 받겠습니다.</p>
+        <p> 비루한 자취생, 취준생 에게 자비를 주소서(리스)</p>
+        <Image
+          style={{ alignSelf: "center", marginTop: "15%" }}
+          src="/qr/sponsor_qr.JPG"
+          alt="카카오페이 후원 QR코드"
+          width={256}
+          height={256}
+        />
+        <p style={{ alignSelf: "center", marginTop: "1%" }}> 방 * 민</p>
+      </div>
     </PageLayout>
   );
 }
